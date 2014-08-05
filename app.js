8fda3bc210084cf0ac533182726c622f1a1a6903
Abraham Silva <abrahamjso@gmail.com>
2014-08-05 23:44:11 +0100

    nothing

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Load Our Modules
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var movies = require('./routes/movies');
@@ -7,15 +6,16 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+
 //connect to our database
 //Ideally you will obtain DB details from a config file
-
 var dbName='movieDB';
 //port :27017
 var connectionString='mongodb://localhost:27017/'+dbName;
-
 mongoose.connect(connectionString);
 
+
+//Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
